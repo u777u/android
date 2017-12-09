34866aba39ccee42453f816c45ab7cbd80438095
nyancodeid <ryandevstudio@gmail.com>
2017-12-09 22:09:48 +0700

    update

---
+++ lang/lang.ptg.js
@@ -0,0 +1,34 @@
+/* DogeFaucet Languange Copyright 2017 
+ *
+ * Portugis
+ * Last update 2017/12/7 
+ */
+
+var Lang = new Language('ptg');
+
+Lang.push('withdrawal_attention', 'Todas as solicitações de retirada são processadas automaticamente imediatamente depois de enviar um pedido de retirada.');
+Lang.push('fee_withdrawal', 'A Taxa (Fee) de retirada é');
+Lang.push('estimated_receive', 'Recebimento Estimado');
+Lang.push('request_withdrawal', 'Pedido de retirada');
+Lang.push('referral_code', 'Código de Referência');
+Lang.push("minimum_withdrawal", "Retirada mínima");
+Lang.push('current_address', 'Endereço atual');
+Lang.push('current_earning', 'Ganhos atuais');
+Lang.push('referral_earn', 'Consulta Ganhe');
+Lang.push('send_request', 'Enviar pedido');
+Lang.push('withdrawal_title', 'Retirada');
+Lang.push('referral_title', 'Referrals');
+Lang.push('history_title', 'Histórico');
+Lang.push('earning_title', 'Earnning');
+Lang.push('claim_now', 'Claim Now');
+Lang.push('donate_us', 'Dona-nos');
+Lang.push('attention', 'Atenção');
+Lang.push('amount', 'Quantidade');
+Lang.push('news', 'Notícias');
+
+Lang.push('success', 'Success');
+Lang.push('pending', 'Pending');
+Lang.push('approved', 'Approved');
+Lang.push('processed', 'Processed');
+
+Lang.render();
